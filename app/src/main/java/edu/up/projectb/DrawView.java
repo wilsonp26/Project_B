125e5458a573d390fee1b2589cfd6a99ef6a2e2b
Alberto Lucero <luceroa29@up.edu>
2026-02-03 21:11:23 -0800

    Created pawns in drawview

--- app/src/main/java/edu/up/projectb/DrawView.java
+++ app/src/main/java/edu/up/projectb/DrawView.java
@@ -29,47 +29,48 @@ private Paint green = new Paint();
         canvas.drawBitmap(board, 0, 130, null);
 
         //This is the beginning of the color declaration section
+        //The format for the Player Pieces is as follows:
+        //Color
         Paint spaceGrey = new Paint();
         spaceGrey.setColor(0xFF343d46);
         spaceGrey.setStyle(Paint.Style.FILL);
+
+        //Head of piece (circle)
+        canvas.drawCircle(420, 473, 15, spaceGrey);
+        //Body of piece (rectangle)
+        canvas.drawRect(415,473, 425, 500,spaceGrey);
 
         Paint red = new Paint();
         red.setColor(Color.RED);
         red.setStyle(Paint.Style.FILL);
 
+        canvas.drawCircle(822, 470, 15, red);
+        canvas.drawRect(817,465, 828, 497,red);
+
         Paint blue = new Paint();
         blue.setColor(Color.BLUE);
         blue.setStyle(Paint.Style.FILL);
+
+        canvas.drawCircle(425, 800, 15, blue);
+        canvas.drawRect(420,800, 430, 830,blue);
 
         Paint green = new Paint();
         green.setColor(Color.GREEN);
         green.setStyle(Paint.Style.FILL);
 
+        canvas.drawCircle(255, 473, 15, green);
+        canvas.drawRect(250,475, 260, 497,green);
+
         Paint yellow = new Paint();
         yellow.setColor(Color.YELLOW);
         yellow.setStyle(Paint.Style.FILL);
 
+        canvas.drawCircle(535, 245, 15, yellow);
+        canvas.drawRect(530,245, 540, 278,yellow);
+
         //end of the declaration section
-
-        //The drawCircles draw the head of the piece, and the rectangles draw the body
-        canvas.drawCircle(420, 250, 15, spaceGrey);
-        canvas.drawRect(415,250, 425, 280,spaceGrey);
-
-        canvas.drawCircle(420, 250, 15, red);
-        canvas.drawRect(415,250, 425, 280,red);
-
-        canvas.drawCircle(420, 250, 15, blue);
-        canvas.drawRect(415,250, 425, 280,blue);
-
-        canvas.drawCircle(420, 250, 15, green);
-        canvas.drawRect(415,250, 425, 280,green);
-
-        canvas.drawCircle(420, 250, 15, yellow);
-        canvas.drawRect(415,250, 425, 280,yellow);
-
     }
 
 }
-
 // Attributions for the assets used in this project:
 // https://www.sprinttosave.com/product-p-674011.html
