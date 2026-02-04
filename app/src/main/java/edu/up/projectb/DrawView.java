c7ab580de8697614623894f9ed23ff82717b2237
Paloma Wilson <wilsonp26@up.edu>
2026-02-03 20:14:25 -0800

    newest thing

--- app/src/main/java/edu/up/projectb/DrawView.java
+++ app/src/main/java/edu/up/projectb/DrawView.java
@@ -1,22 +1,17 @@
 
 package edu.up.projectb;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceView;
-import android.widget.ImageView;
 
 public class DrawView extends SurfaceView {
     private Bitmap board =
             BitmapFactory.decodeResource(getResources(), R.drawable.coloredboard);
 
-  //  private Bitmap artTwo =
-  //          BitmapFactory.decodeResource(getResources(), R.drawable.arttwo);
 private Paint green = new Paint();
 
     public DrawView(Context context, AttributeSet attrs) {
@@ -25,15 +20,10 @@ private Paint green = new Paint();
         green.setColor(0xFF00FF00);
         green.setStyle(Paint.Style.FILL);
         board = Bitmap.createScaledBitmap(board, 1112, 834, false);
-       // artTwo = Bitmap.createScaledBitmap(artTwo, 50, 60, false);
     }
     @Override
     public void onDraw(Canvas canvas) {
-      //  canvas.drawRect(0, 0, 4000, 4000, green);
-        //canvas.drawBitmap(artTwo, 0, 130, null);
         canvas.drawBitmap(board, 0, 130, null);
-
-
     }
 
 }
